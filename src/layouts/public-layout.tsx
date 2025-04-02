2b1c4f027a33759a0358d3913fb349ad83c5fbfc
Memon Zaid <memonzaid4444@gmail.com>
2025-04-02 15:26:22 +0530

    Changes: Clerk Authentication and Routing Setup Completed

---
+++ src/layouts/public-layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/footer"
+import Header from "@/components/header"
+import { Outlet } from "react-router-dom"
+
+export const PublicLayout = () => {
+    return (
+        <div className="w-full">
+            {/* Handler to store the user data */}
+            <Header />
+
+            <Outlet />
+
+            <Footer />
+
+        </div>
+    )
+}
+
